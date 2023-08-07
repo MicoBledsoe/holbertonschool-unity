0866977b747058f9df5b7c3035523871428399e9
MicoBledsoe <micomods@gmail.com>
2023-08-07 11:33:26 -0500

    fix

--- unity-animation/Assets/Scripts/PlayerController.cs
+++ unity-animation/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float turnSpeed = 200f; 
+    public float turnSpeed = 200f;
     public float jumpForce = 5f;
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
@@ -16,18 +17,19 @@ public class PlayerController : MonoBehaviour
     private bool isFalling = false;
     private Rigidbody rb;
     private Vector3 movement;
+    private bool isGrounded = false;
+    private bool isJumpKeyPressed = false;
     [SerializeField]
-    private Animator animator;  
+    private Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        //animator = GetComponent<Animator>();  
     }
 
     void Start()
     {
-        IsGrounded();
+        StartCoroutine(CheckGroundedStatus());
     }
 
     private void Update()
@@ -37,23 +39,18 @@ public class PlayerController : MonoBehaviour
 
         movement = new Vector3(horizontalMovement, 0f, verticalMovement) * moveSpeed;
 
-        if (horizontalMovement != 0) {
+        if (horizontalMovement != 0)
+        {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement.normalized), Time.deltaTime * turnSpeed);
         }
 
-        if (movement != Vector3.zero) {
-            animator.SetBool("isRunning", true);
-            Debug.Log("Running animation should be playing");  // Debug statement 1
-        } else {
-            animator.SetBool("isRunning", false);
-            Debug.Log("Idle animation should be playing");  // Debug statement 2
-        }
-
-        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        if (movement != Vector3.zero)
         {
-            isJumping = true;
-            isFalling = false;
-            Debug.Log("Jump");
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
         }
 
         if (transform.position.y < respawnHeight)
@@ -61,20 +58,46 @@ public class PlayerController : MonoBehaviour
             Respawn();
         }
 
-        if (rb.velocity.y < 0 && !isJumping)
+        if (!isGrounded && rb.velocity.y < -0.5f && !isJumping)
         {
             isFalling = true;
+            animator.SetBool("isFalling", true);
+            Debug.Log("Falling animation should be playing");
+            Debug.Log("Started falling at time: " + Time.time);
         }
+        else
+        {
+            isFalling = false;
+            animator.SetBool("isFalling", false);
+        }
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumpKeyPressed = true;
+            animator.SetBool("isJumping", true);
+            isFalling = false;
+        }
+
+        if (isGrounded && isFalling && Vector3.Distance(transform.position, startPosition) < 0.1f)
+        {
+            animator.SetBool("FallingToImpact", true);
+        }
+        else
+        {
+            animator.SetBool("FallingToImpact", false);
+        }
+
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
 
-        if (isJumping)
+        if (isJumpKeyPressed)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
-            isJumping = false;
+            isJumping = true; // Indicate that jump has started
+            isJumpKeyPressed = false; // Reset the flag
         }
     }
 
@@ -84,18 +107,33 @@ public class PlayerController : MonoBehaviour
         rb.useGravity = false;
         transform.position = startPosition + Vector3.up * respawnOffset;
         rb.useGravity = true;
+
         isFalling = false;
+        isJumping = false;
+        isGrounded = true;
+        animator.SetBool("isJumping", false);
+        animator.SetBool("isFalling", false);
+        animator.SetBool("isRunning", false);
+        animator.SetBool("isGrounded", true);
+        Debug.Log($"Respawning. isFalling: {isFalling}, isJumping: {isJumping}, isGrounded: {isGrounded}");
+        animator.Play("Falling Flat Impact");
     }
 
-    private bool IsGrounded()
+    private IEnumerator CheckGroundedStatus()
     {
-        bool grounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
-        if (grounded)
+        while (true)
         {
-            Debug.Log("Grounded");
-            isJumping = false;
-            isFalling = false;
+            yield return new WaitForSeconds(0.1f);
+
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+            if (isGrounded)
+            {
+                Debug.Log("Grounded");
+                isJumping = false;
+                isFalling = false;
+                animator.SetBool("isJumping", false); // Reset isJumping to false in animator
+                animator.SetBool("isFalling", false); // Reset isFalling to false in animator
+            }
         }
-        return grounded;
     }
 }
