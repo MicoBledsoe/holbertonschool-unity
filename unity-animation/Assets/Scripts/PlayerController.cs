bf7fd0286f01a8972959c9052793526e28d9e1e8
MicoBledsoe <micomods@gmail.com>
2023-08-18 09:36:28 -0500

    push new repo

--- unity-animation/Assets/Scripts/PlayerController.cs
+++ unity-animation/Assets/Scripts/PlayerController.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
     private Vector3 movement;
     private bool isGrounded = false;
     private bool isJumpKeyPressed = false;
-    [SerializeField]
+    [SerializeField] // protection of data
     private Animator animator;
 
     private void Awake()
